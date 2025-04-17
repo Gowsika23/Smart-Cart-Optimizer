da7b54ee3f2b8968a28706311138ab58bdf20218
Gowsika <134724663+Gowsika23@users.noreply.github.com>
2025-04-18 00:27:31 +0530

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>&copy; 2025 Smart Cart | Built with ❤️ & AI</p>
-    </footer>
-  );
-}
